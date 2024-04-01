289f36046f2e83955cc86f82c79978671b73cc8f
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2024-04-01 17:55:01 +0530

    working with ec2 instances added

---
+++ 6-ec2_1.py
@@ -0,0 +1,6 @@
+# Describe ec2 instances
+import boto3
+
+ec2 = boto3.client('ec2')
+response = ec2.describe_instances()
+print(response) 
---
+++ 7-ec2_2.py
@@ -0,0 +1,10 @@
+# Moniter and unmonitor EC2 instances
+import sys
+import boto3
+
+ec2 = boto3.client('ec2')
+if sys.argv[1] == 'ON':
+    response = ec2.monitor_instances(InstanceIds=['INSTANCE_ID'])
+else:
+    response = ec2.unmonitor_instances(InstanceIds=['INSTANCE_ID'])
+print(response)
